c9a7bfea3fc28be19207e63da57f9d5a0e637e23
Kim-AYoung <onionlily123@naver.com>
2022-07-21 23:05:17 +0900

    init project

---
+++ order/src/main/java/org/prgms/kdt/order/OrderApplication.java
@@ -0,0 +1,13 @@
+package org.prgms.kdt.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+
+}
