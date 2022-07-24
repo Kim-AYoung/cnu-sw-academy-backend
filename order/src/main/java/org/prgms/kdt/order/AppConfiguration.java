ede6490e8b039e75ba7a6941ab9ea3a448c69209
Kim-AYoung <onionlily123@naver.com>
2022-07-24 23:38:29 +0900

    bean lifecycle callbacks

--- order/src/main/java/org/prgms/kdt/order/AppConfiguration.java
+++ order/src/main/java/org/prgms/kdt/order/AppConfiguration.java
@@ -1,10 +1,20 @@
 package org.prgms.kdt.order;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan
+@ComponentScan(basePackages = {"org.prgms.kdt.order.order", "org.prgms.kdt.order.voucher", "org.prgms.kdt.order.configuration"})
 public class AppConfiguration {
+    @Bean(initMethod = "init")
+    public BeanOne beanOne() {
+        return new BeanOne();
+    }
+}
 
+class BeanOne{
+    public void init() {
+        System.out.println("[BeanOne] init called");
+    }
 }
--- order/src/main/java/org/prgms/kdt/order/voucher/MemoryVoucherRepository.java
+++ order/src/main/java/org/prgms/kdt/order/voucher/MemoryVoucherRepository.java
@@ -1,14 +1,20 @@
 package org.prgms.kdt.order.voucher;
 
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Repository
-public class MemoryVoucherRepository implements VoucherRepository {
+@Qualifier("memory")
+public class MemoryVoucherRepository implements VoucherRepository, InitializingBean, DisposableBean {
 
     private final Map<UUID, Voucher> storage = new ConcurrentHashMap<>();
 
@@ -21,5 +27,23 @@ public class MemoryVoucherRepository implements VoucherRepository {
     public Voucher insert(Voucher voucher) {
         storage.put(voucher.getVoucherId(), voucher);
         return voucher;
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        System.out.println("[MemoryVoucherRepository] postConstruct called");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("[MemoryVoucherRepository] afterPropertiesSet called");
+    }
+
+    @PreDestroy
+    public void preDestory() { System.out.println("[MemoryVoucherRepository] preDestroy called"); }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("[MemoryVoucherRepository] destroy called");
     }
 }
