9ce3b11f6c7fdc02e5c4ec985e46cf58d11dc6d8
Kim-AYoung <onionlily123@naver.com>
2022-07-24 23:54:38 +0900

    test circular dependency

---
+++ order/src/main/java/org/prgms/kdt/order/CircularDepTester.java
@@ -0,0 +1,43 @@
+package org.prgms.kdt.order;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+class A {
+    private final B b;
+
+    A(B b) {
+        this.b = b;
+    }
+}
+
+class B {
+
+    private final A a;
+
+    B(A a) {
+        this.a = a;
+    }
+}
+
+@Configuration
+class CircularConfig {
+    @Bean
+    public A a(B b) {
+        return new A(b);
+    }
+
+    @Bean
+    B b(A a) {
+        return new B(a);
+    }
+}
+
+public class CircularDepTester {
+
+    public static void main(String[] args) {
+        var annotationConfigApplicationContext = new AnnotationConfigApplicationContext(CircularConfig.class);
+    }
+
+}
