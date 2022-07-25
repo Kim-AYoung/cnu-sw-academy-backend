c4898fc855d9cacb0e59ba00974db29d744517e0
Kim-AYoung <onionlily123@naver.com>
2022-07-25 16:35:28 +0900

    add, use properties

--- order/src/main/java/org/prgms/kdt/order/AppConfiguration.java
+++ order/src/main/java/org/prgms/kdt/order/AppConfiguration.java
@@ -3,18 +3,11 @@ package org.prgms.kdt.order;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan(basePackages = {"org.prgms.kdt.order.order", "org.prgms.kdt.order.voucher", "org.prgms.kdt.order.configuration"})
+@PropertySource("application.properties")
 public class AppConfiguration {
-    @Bean(initMethod = "init")
-    public BeanOne beanOne() {
-        return new BeanOne();
-    }
-}
 
-class BeanOne{
-    public void init() {
-        System.out.println("[BeanOne] init called");
-    }
 }
--- order/src/main/java/org/prgms/kdt/order/OrderTester.java
+++ order/src/main/java/org/prgms/kdt/order/OrderTester.java
@@ -6,16 +6,19 @@ import org.prgms.kdt.order.voucher.FixedAmountVoucher;
 import org.prgms.kdt.order.voucher.VoucherRepository;
 import org.springframework.beans.factory.annotation.BeanFactoryAnnotationUtils;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 import org.springframework.util.Assert;
 
 import java.text.MessageFormat;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public class OrderTester {
     public static void main(String[] args) {
 
         var applicationContext = new AnnotationConfigApplicationContext(AppConfiguration.class);
+
         var orderService = applicationContext.getBean(OrderService.class);
 
         var customerId = UUID.randomUUID();
@@ -28,6 +31,14 @@ public class OrderTester {
         var order = orderService.createOrder(UUID.randomUUID(), orderItems, voucher.getVoucherId());
 
         Assert.isTrue(order.totalAmount() == 90L, MessageFormat.format("totalAmount {0} is not 90L", order.totalAmount()));
+
+        var environment = applicationContext.getEnvironment();
+        var version = environment.getProperty("kdt.version");
+        var minimumOrderAmount = environment.getProperty("kdt.minimum-order-amount", Integer.class);
+        var supportVendors = environment.getProperty("kdt.support-vendors", List.class);
+        System.out.println(MessageFormat.format("version -> {0}", version));
+        System.out.println(MessageFormat.format("minimumOrderAmount -> {0}", minimumOrderAmount));
+        System.out.println(MessageFormat.format("supportVendors -> {0}", supportVendors));
 
         applicationContext.close();
     }
