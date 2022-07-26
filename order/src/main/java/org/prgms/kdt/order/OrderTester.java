fb4741440105ab60fb308c06c55980ac288f994d
Kim-AYoung <onionlily123@naver.com>
2022-07-26 14:37:26 +0900

    logging using ColorConverter

--- order/src/main/java/org/prgms/kdt/order/OrderTester.java
+++ order/src/main/java/org/prgms/kdt/order/OrderTester.java
@@ -10,6 +10,8 @@ import org.prgms.kdt.order.voucher.VoucherRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.BeanFactoryAnnotationUtils;
+import org.springframework.boot.ansi.AnsiColor;
+import org.springframework.boot.ansi.AnsiOutput;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.core.io.Resource;
 import org.springframework.util.Assert;
@@ -32,6 +34,8 @@ public class OrderTester {
     private static final Logger logger = LoggerFactory.getLogger(OrderTester.class);
 
     public static void main(String[] args) throws IOException {
+
+        AnsiOutput.setEnabled(AnsiOutput.Enabled.ALWAYS);
 
         var applicationContext = new AnnotationConfigApplicationContext();
         applicationContext.register(AppConfiguration.class);
