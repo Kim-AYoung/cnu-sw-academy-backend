c9db8a42ae23737c03b58e0bc1a08c8c17faf751
Kim-AYoung <onionlily123@naver.com>
2022-08-05 14:13:21 +0900

    create TestServlet

---
+++ order/src/main/java/org/prgms/kdt/order/servlet/TestServlet.java
@@ -0,0 +1,30 @@
+package org.prgms.kdt.order.servlet;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class TestServlet extends HttpServlet {
+
+    private static final Logger logger = LoggerFactory.getLogger(TestServlet.class);
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        logger.info("Init Servlet");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        var requestURI = req.getRequestURI();
+        logger.info("Get request from {}", requestURI);
+
+        var writer = resp.getWriter();
+        writer.println("Hello Servlet!");
+    }
+}
