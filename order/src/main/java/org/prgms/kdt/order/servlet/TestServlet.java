a3108da07cf206c1b7048e53a991d95c2e630846
Kim-AYoung <onionlily123@naver.com>
2022-08-08 15:43:24 +0900

    register servlet using WebServlet annotation in spring

--- order/src/main/java/org/prgms/kdt/order/servlet/TestServlet.java
+++ order/src/main/java/org/prgms/kdt/order/servlet/TestServlet.java
@@ -4,11 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet(value = "/*", loadOnStartup = 1)
 public class TestServlet extends HttpServlet {
 
     private static final Logger logger = LoggerFactory.getLogger(TestServlet.class);
