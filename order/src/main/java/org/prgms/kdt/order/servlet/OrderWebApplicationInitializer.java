fc85d88aac4a94cbfe2875ebb5e925124c123311
Kim-AYoung <onionlily123@naver.com>
2022-08-08 15:51:51 +0900

    implements WebApplicationInitializer and register DispatcherServlet

--- order/src/main/java/org/prgms/kdt/order/servlet/OrderWebApplicationInitializer.java
+++ order/src/main/java/org/prgms/kdt/order/servlet/OrderWebApplicationInitializer.java
@@ -1,22 +1,90 @@
 package org.prgms.kdt.order.servlet;
 
+import com.zaxxer.hikari.HikariDataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.EncodedResourceResolver;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.sql.DataSource;
 
 public class OrderWebApplicationInitializer implements WebApplicationInitializer {
 
     private static final Logger logger = LoggerFactory.getLogger(OrderWebApplicationInitializer.class);
 
+    @Configuration
+    @EnableWebMvc
+    @ComponentScan(basePackages = "org.prgms.kdt.order.customer")
+    @EnableTransactionManagement
+    static class AppConfig implements WebMvcConfigurer {
+
+        @Override
+        public void configureViewResolvers(ViewResolverRegistry registry) {
+            registry.jsp();
+        }
+
+        @Override
+        public void addResourceHandlers(ResourceHandlerRegistry registry) {
+            registry.addResourceHandler("/resources/**")
+                    .addResourceLocations("/resources/")
+                    .resourceChain(true)
+                    .addResolver(new EncodedResourceResolver());
+        }
+
+        @Bean
+        public DataSource dataSource() {
+            var dataSource = DataSourceBuilder.create()
+                    .url("jdbc:mysql://localhost/order_mgmt")
+                    .username("root")
+                    .password("")
+                    .type(HikariDataSource.class)
+                    .build();
+            return dataSource;
+        }
+
+        @Bean
+        public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+            return new JdbcTemplate(dataSource);
+        }
+
+        @Bean
+        public NamedParameterJdbcTemplate namedParameterJdbcTemplate(JdbcTemplate jdbcTemplate) {
+            return new NamedParameterJdbcTemplate(jdbcTemplate);
+        }
+
+        @Bean
+        public PlatformTransactionManager platformTransactionManager(DataSource dataSource) {
+            return new DataSourceTransactionManager(dataSource);
+        }
+    }
+
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
         logger.info("Starting Server...");
 
-        var servletRegistration = servletContext.addServlet("test", new TestServlet());
-        servletRegistration.addMapping("/*");
+        var applicationContext = new AnnotationConfigWebApplicationContext();
+        applicationContext.register(AppConfig.class);
+
+        var dispatcherServlet = new DispatcherServlet(applicationContext);
+        var servletRegistration = servletContext.addServlet("test", dispatcherServlet);
+        servletRegistration.addMapping("/");
         servletRegistration.setLoadOnStartup(1);
     }
 }
