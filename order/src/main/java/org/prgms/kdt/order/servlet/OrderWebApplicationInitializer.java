f80a628d66a4368ca8fd0ec3eafb1f3ee6144ca0
Kim-AYoung <onionlily123@naver.com>
2022-08-10 18:30:13 +0900

    add CORS mapping

--- order/src/main/java/org/prgms/kdt/order/servlet/OrderWebApplicationInitializer.java
+++ order/src/main/java/org/prgms/kdt/order/servlet/OrderWebApplicationInitializer.java
@@ -29,10 +29,7 @@ import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.resource.EncodedResourceResolver;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
@@ -101,6 +98,13 @@ public class OrderWebApplicationInitializer implements WebApplicationInitializer
             javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ISO_DATE_TIME));
             var modules = Jackson2ObjectMapperBuilder.json().modules(javaTimeModule);
             converters.add(0, new MappingJackson2HttpMessageConverter(modules.build()));
+        }
+
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/api/**")
+                    .allowedMethods("GET", "POST")
+                    .allowedOrigins("*");
         }
     }
 
