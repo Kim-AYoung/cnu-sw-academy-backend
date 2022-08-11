e0614716c5e1f7d9dec828f1c34534c98d3ecf33
Kim-AYoung <onionlily123@naver.com>
2022-08-11 17:16:19 +0900

    change from spring to spring boot project and use auto configuration of spring boot

--- order/src/main/java/org/prgms/kdt/order/OrderApplication.java
+++ order/src/main/java/org/prgms/kdt/order/OrderApplication.java
@@ -1,28 +1,13 @@
 package org.prgms.kdt.order;
 
-import org.prgms.kdt.order.configuration.YamlPropertiesFactory;
-import org.prgms.kdt.order.order.OrderItem;
-import org.prgms.kdt.order.order.OrderProperties;
-import org.prgms.kdt.order.order.OrderService;
-import org.prgms.kdt.order.voucher.FixedAmountVoucher;
-import org.prgms.kdt.order.voucher.JDBCVoucherRepository;
-import org.prgms.kdt.order.voucher.VoucherRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.util.Assert;
-
-import java.text.MessageFormat;
-import java.util.ArrayList;
-import java.util.UUID;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"org.prgms.kdt.order.order", "org.prgms.kdt.order.voucher"})
+@ComponentScan(basePackages = {"org.prgms.kdt.order.customer", "org.prgms.kdt.order.config"})
 public class OrderApplication {
     public static void main(String[] args) {
         SpringApplication.run(OrderApplication.class, args);
-
     }
 }
