4ff41e0cb476cd0d08bce3bb3fdb808508556fa1
Kim-AYoung <onionlily123@naver.com>
2022-08-12 21:27:12 +0900

    init Springboot Project

---
+++ springboot-jpa-lecture/src/main/java/org/prgms/kdt/springbootjpalecture/SpringbootJpaLectureApplication.java
@@ -0,0 +1,13 @@
+package org.prgms.kdt.springbootjpalecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJpaLectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJpaLectureApplication.class, args);
+    }
+
+}
