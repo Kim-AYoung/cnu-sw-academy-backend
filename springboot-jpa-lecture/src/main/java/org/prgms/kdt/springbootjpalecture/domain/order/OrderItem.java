5994e61b4406bb73094ada0a559c4f9968b3779f
Kim-AYoung <onionlily123@naver.com>
2022-08-19 02:58:03 +0900

    연관관계 설정

--- springboot-jpa-lecture/src/main/java/org/prgms/kdt/springbootjpalecture/domain/order/OrderItem.java
+++ springboot-jpa-lecture/src/main/java/org/prgms/kdt/springbootjpalecture/domain/order/OrderItem.java
@@ -17,9 +17,11 @@ public class OrderItem {
     private int price;
     private int quantity;
 
+    @ManyToOne
     @JoinColumn(name = "order_id", referencedColumnName = "id")
     private Order order;
 
+    @ManyToOne
     @JoinColumn(name = "item_id", referencedColumnName = "id")
     private Item item;
 
