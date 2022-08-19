3571488ac7e3e33d7536cb9f32b3fe8e418c3979
Kim-AYoung <onionlily123@naver.com>
2022-08-19 16:32:42 +0900

    change order proxy

--- springboot-jpa-lecture/src/main/java/org/prgms/kdt/springbootjpalecture/domain/order/Order.java
+++ springboot-jpa-lecture/src/main/java/org/prgms/kdt/springbootjpalecture/domain/order/Order.java
@@ -26,7 +26,7 @@ public class Order extends BaseTimeEntity{
     @Lob
     private String memo;
 
-    @ManyToOne(fetch = FetchType.EAGER)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id", referencedColumnName = "id")
     private Member member;
 
