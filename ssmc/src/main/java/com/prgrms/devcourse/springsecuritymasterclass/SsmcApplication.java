63315689d7b2b2343b105e8a561183a6c4342ff1
Kim-AYoung <onionlily123@naver.com>
2022-08-23 17:14:48 +0900

    init project

---
+++ ssmc/src/main/java/com/prgrms/devcourse/springsecuritymasterclass/SsmcApplication.java
@@ -0,0 +1,13 @@
+package com.prgrms.devcourse.springsecuritymasterclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsmcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsmcApplication.class, args);
+    }
+
+}
---
+++ ssmc/src/test/java/com/prgrms/devcourse/springsecuritymasterclass/SsmcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prgrms.devcourse.springsecuritymasterclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsmcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
