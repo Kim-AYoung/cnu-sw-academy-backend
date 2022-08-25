c7a7cfa90e21c8fcacec4bbca8bec2dd39f5d1f2
Kim-AYoung <onionlily123@naver.com>
2022-08-25 17:17:07 +0900

    add spring security

---
+++ ssmc/src/main/java/com/prgrms/devcourse/springsecuritymasterclass/configure/WebSecurityConfigure.java
@@ -0,0 +1,49 @@
+package com.prgrms.devcourse.springsecuritymasterclass.configure;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("{noop}user123").roles("USER").and()
+                .withUser("admin").password("{noop}admin123").roles("ADMIN")
+        ;
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/assets/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+                .antMatchers("/me").hasAnyRole("USER", "ADMIN")
+                .anyRequest().permitAll()
+                .and()
+            .formLogin()
+                .defaultSuccessUrl("/")
+                .permitAll()
+                .and()
+            .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/")
+                .invalidateHttpSession(true)
+                .clearAuthentication(true)
+                .and()
+            .rememberMe()
+                .rememberMeParameter("remember-me")
+                .tokenValiditySeconds(300)
+        ;
+    }
+}
