df5ecae9d00aca2a51069d1d25150173eaa30c27
Kim-AYoung <onionlily123@naver.com>
2022-08-26 00:27:57 +0900

    create admin page and accessDeniedHandler

--- ssmc/src/main/java/com/prgrms/devcourse/springsecuritymasterclass/configure/WebMvcConfigure.java
+++ ssmc/src/main/java/com/prgrms/devcourse/springsecuritymasterclass/configure/WebMvcConfigure.java
@@ -11,5 +11,6 @@ public class WebMvcConfigure implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("index");
         registry.addViewController("/me").setViewName("me");
+        registry.addViewController("/admin").setViewName("admin");
     }
 }
--- ssmc/src/main/java/com/prgrms/devcourse/springsecuritymasterclass/configure/WebSecurityConfigure.java
+++ ssmc/src/main/java/com/prgrms/devcourse/springsecuritymasterclass/configure/WebSecurityConfigure.java
@@ -1,16 +1,27 @@
 package com.prgrms.devcourse.springsecuritymasterclass.configure;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.servlet.http.HttpServletResponse;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
@@ -29,6 +40,7 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
         http
             .authorizeRequests()
                 .antMatchers("/me").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/admin").access("hasRole('ADMIN') and isFullyAuthenticated()")
                 .anyRequest().permitAll()
                 .and()
             .formLogin()
@@ -51,6 +63,25 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
             .anonymous()
                 .principal("thisIsAnonymousUser")
                 .authorities("ROLE_ANONYMOUS", "ROLE_UNKNOWN")
+                .and()
+            .exceptionHandling()
+                .accessDeniedHandler(accessDeniedHandler())
         ;
+    }
+
+    @Bean
+    public AccessDeniedHandler accessDeniedHandler() {
+        return (request, response, accessDeniedException) -> {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            Object principal = authentication != null ? authentication.getPrincipal() : null;
+
+            log.warn("{} is denied", principal, accessDeniedException);
+
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            response.setContentType("text/plain");
+            response.getWriter().write("## ACCESS DENIED ##");
+            response.getWriter().flush();
+            response.getWriter().close();
+        };
     }
 }
